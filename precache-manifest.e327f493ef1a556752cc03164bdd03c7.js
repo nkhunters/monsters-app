1aeb4c75db64f7bc7474f8081bb402529a8a2eed
Satyam <nkhunters07@gmail.com>
2019-08-16 03:36:12 +0530

    Updates

---
+++ precache-manifest.e327f493ef1a556752cc03164bdd03c7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "889a2857794a239a4a15248f2ef9a897",
+    "url": "/monsters-app/index.html"
+  },
+  {
+    "revision": "ed1439ed142cfcd3a9db",
+    "url": "/monsters-app/static/css/main.1130f606.chunk.css"
+  },
+  {
+    "revision": "90bc354edad42df017e0",
+    "url": "/monsters-app/static/js/2.08bb4953.chunk.js"
+  },
+  {
+    "revision": "ed1439ed142cfcd3a9db",
+    "url": "/monsters-app/static/js/main.5a8d2f25.chunk.js"
+  },
+  {
+    "revision": "eb5a9f089e936315e0d4",
+    "url": "/monsters-app/static/js/runtime~main.bc32956a.js"
+  }
+]);
